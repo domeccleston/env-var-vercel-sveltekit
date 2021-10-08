87204afbc6e695eb8cf63fb5cd64bef372902cd0
Dom Eccleston <domeccleston@gmail.com>
2021-10-08 08:28:35 +0100

    Set up SvelteKit project to test exposing env vars

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Due to how Vite works, we can't import variables directly into .svelte files, so we have to import them from a .js file.
+  // See https://kit.svelte.dev/faq#env-vars
+  import { VITE_VERCEL_ENV } from "../env";
+</script>
+
+<code>VITE_VERCEL_ENV: <b>{VITE_VERCEL_ENV}</b></code>
+<p>
+  If this SvelteKit project is deployed with the zero-config SvelteKit preset,
+  the above variable will be undefined.
+</p>
+<p>
+  If I change the framework preset to Vite, VITE_VERCEL_ENV will be defined, but
+  that makes me lose the zero-config benefit as I have to override build
+  commands, output directory etc.
+</p>
